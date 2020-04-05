ee74d08ee69dfe8dd7c4945ad9e4b6b337a15e73
sandeep.jain <sandeep.jain@indianexpress.com>
2020-04-05 20:26:08 +0530

    add event emitter

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('in app');
+//https://medium.com/developers-arena/nodejs-event-emitters-for-beginners-and-for-experts-591e3368fdd2.
+const events = require('events');
+
+const eventemitter = new events.EventEmitter();
+eventemitter.on('myFirstEvent',()=>{
+	console.log('my first event');
+});
+eventemitter.emit('myFirstEvent');
